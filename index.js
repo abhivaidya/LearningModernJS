16bf9cc5b393dba48260be5f2884aa8ad4b3b3ff
Abhishek <abhivaidya@gmail.com>
2018-05-17 19:04:21 +0530

    Some extra things added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var test = "Abhishek";
+//Strings
+let firstName = "Abhishek";
+let lastName = "Vaidyanathan";
+let fullName = firstName + " " + lastName;
 
-console.log(test);
+let city = "Navi Mumbai";
+let country = "India";
+let loc = city + ", " + country;
+
+console.log(fullName);
+console.log(loc);
+
+//Numbers
+let studentScore = 18;
+let maxScore = 20;
+
+console.log(studentScore/maxScore * 100);
